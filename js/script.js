d5e5f2ba9ead2c2fdfd850f8269ce397bfca9d6e
Aleksey Belchenkov <u608110@gmail.com>
2021-01-14 12:23:31 +0300

    Auto Text Effect

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
+const textEl = document.getElementById('text');
+const speedEl = document.getElementById('speed');
 
+const text = 'JavaScript is a cool Programming Language!!!';
+let idx = 1;
+let speed = 300 / speedEl.value;
+
+writeText();
+
+speedEl.addEventListener('input', e => speed = 300 / e.target.value);
+
+function writeText() {
+    textEl.innerText = text.slice(0, idx);
+    idx++;
+
+    if (idx > text.length) {
+        idx = 1;
+    }
+
+    setTimeout(writeText, speed);
+}
